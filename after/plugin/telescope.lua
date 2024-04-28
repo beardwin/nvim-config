55e69891980a50a757e45bc03649fba6b8280b21
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-04-28 12:13:07 -0700

    add vim-test

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,13 @@
 local builtin = require('telescope.builtin')
+
+require('telescope').setup({
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+		}
+	}
+})
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
---
+++ after/plugin/test.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>tst", ":TestNearest<cr>")
+vim.keymap.set("n", "<leader>tsf", ":TestFile<cr>")
+vim.keymap.set("n", "<leader>tsa", ":TestSuite<cr>")
+vim.keymap.set("n", "<leader>tsv", ":TestVisit<cr>")
