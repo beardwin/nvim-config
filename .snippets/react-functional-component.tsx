10eb006b9e01a17fc9346c9e4fdcb248ec3288f1
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-04-28 21:39:44 -0700

    add snippets

---
+++ .snippets/react-functional-component.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+export interface Props {
+
+}
+
+export const $ComponentName$ = ({}: Props) => {
+  return null
+}
+
+const styles = StyleSheet.create({
+
+})
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>rxfc", ":read ~/.config/nvim/.snippets/react-functional-component.tsx<cr>", {noremap = true})
