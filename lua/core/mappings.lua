345b018bce786a6d6c24bfb20676b4f2f23a53b4
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-05-10 10:27:33 -0700

    can't remember, but it's awesome.

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,5 +1,8 @@
+-- custom mappings
 vim.g.mapleader = " "
--- vim.keymap.set("n", "pv", vim.cmd.Ex)
+vim.keymap.set("i", "<C-s>", "<ESC>:w<cr>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- snippets
 vim.keymap.set("n", "<leader>rxfc", ":read ~/.config/nvim/.snippets/react-functional-component.tsx<cr>", {noremap = true})
 vim.keymap.set("n", "<leader>tsi", ":read ~/.config/nvim/.snippets/typescript-interface.ts<cr>", {noremap = true})
