20dd39a2167efb4d547bcb0e50f6fbe5616012f6
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-05-28 14:19:36 -0700

    visual yank

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,3 +7,15 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.wo.relativenumber = true
 vim.wo.wrap = false
+ 
+-- Create an autocmd for the TextYankPost event
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    -- Highlight the yanked region
+    vim.highlight.on_yank({
+      higroup = 'Visual',
+      timeout = 200,
+    })
+  end
+})
+
