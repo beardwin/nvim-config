8011346eab66b13424c9784df9acea922eb5aacd
Ryan Baldwin <ryan.baldwin@shopify.com>
2024-06-26 14:32:49 -0400

    rainbo & delete other buffers

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,10 +1,11 @@
 -- custom mappings
 vim.g.mapleader = " "
 vim.keymap.set("i", "<C-s>", "<ESC>:w<cr>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>y", "\"+y<cr>")
 vim.keymap.set("v", "<leader>y", "\"+y<cr>")
 vim.keymap.set("n", "<leader>Y", "\"+yy<cr>")
+vim.keymap.set("n", "<leader>o", ":%bd|e#|bd#<cr>")
 
 -- snippets
 vim.keymap.set("n", "<leader>rxfc", ":read ~/.config/nvim/.snippets/react-functional-component.tsx<cr>", {noremap = true})
