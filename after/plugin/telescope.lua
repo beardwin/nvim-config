ca577cce685f683cdb8a08f764dbcd9108cdec19
Ryan Baldwin <ryan.baldwin@shopify.com>
2024-07-09 21:25:13 -0700

    better live grep support

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -22,7 +22,9 @@ require('telescope').setup({
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', function() 
+  builtin.grep_string({search = vim.fn.input("Grep > ")}) end
+  , {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fsh', builtin.search_history, {})
