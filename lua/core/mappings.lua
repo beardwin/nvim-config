99b641551f0742c6378922e25756df1ecb1663fd
Ryan Baldwin <ryan.baldwin@shopify.com>
2024-09-18 06:46:14 -0700

    add Outline.nvim

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -5,7 +5,7 @@ vim.keymap.set("i", "<C-s>", "<ESC>:w<cr>")
 vim.keymap.set("n", "<leader>y", "\"+y<cr>")
 vim.keymap.set("v", "<leader>y", "\"+y<cr>")
 vim.keymap.set("n", "<leader>Y", "\"+yy<cr>")
-vim.keymap.set("n", "<leader>o", ":%bd|e#|bd#<cr>")
+vim.keymap.set("n", "<leader>wo", ":%bd|e#|bd#<cr>")
 
 -- snippets
 vim.keymap.set("n", "<leader>rxfc", ":read ~/.config/nvim/.snippets/react-functional-component.tsx<cr>", {noremap = true})
