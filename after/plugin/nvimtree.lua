cf2ae73ac3e6510486f4337de82c5f4b7423f5f4
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-11-02 12:26:46 -0700

    add custom filters to nvimtree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -18,9 +18,7 @@ require("nvim-tree").setup({
   renderer = {
     group_empty = true,
   },
-  filters = {
-    dotfiles = true,
-  },
+    filters = { dotfiles = false, custom = { '^.git$', '^.vscode$' }},
 	trash = {
 		cmd = "trash"
 	},
