1b2aae082b42361832e9460867e16bbc81bcb871
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-12-04 06:27:14 -0800

    add graphql parser

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -54,3 +54,19 @@ require('mason-lspconfig').setup({
 	}
 })
 
+local gopls_on_attach = function(client, bufnr)
+    if client.name == 'gopls' then
+      client.server_capabilities.semanticTokensProvider = {
+        full = true,
+        legend = {
+          tokenTypes = { 'namespace', 'type', 'class', 'enum', 'interface', 'struct', 'typeParameter', 'parameter', 'variable', 'property', 'enumMember', 'event', 'function', 'method', 'macro', 'keyword', 'modifier', 'comment', 'string', 'number', 'regexp', 'operator', 'decorator' },
+          tokenModifiers = { 'declaration', 'definition', 'readonly', 'static', 'deprecated', 'abstract', 'async', 'modification', 'documentation', 'defaultLibrary'}
+        }
+      }
+    end
+  end
+
+local lspconfig = require('lspconfig')
+lspconfig.gopls.setup({
+  on_attach = gopls_on_attach
+})
---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,20 @@
+require('nvim-treesitter.configs').setup({
+    ensure_installed = { "go" },
+    highlight = {
+        enable = true,
+    },
+})
+
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = { "go", "lua", "javascript" }, -- Add Go here
+                highlight = { enable = true },
+            })
+        end,
+    },
+}
+
