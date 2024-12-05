bdd113eb1c6cd7d9a9e8401ee2340fef56ead696
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-12-04 16:34:36 -0800

    finally got go working with everything

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,20 +2,11 @@ require('nvim-treesitter.configs').setup({
     ensure_installed = { "go" },
     highlight = {
         enable = true,
-        disable = "help"
     },
 })
 
 return {
-    {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = { "go", "lua", "javascript" }, -- Add Go here
-                highlight = { enable = true },
-            })
-        end,
-    },
-}
+    }
 
--- lua/core/plugins/rainbow-delimiters.lua
+++ lua/core/plugins/rainbow-delimiters.lua
@@ -1,1 +1,1 @@
-return {'HiPhish/rainbow-delimiters.nvim', version = "0.4.0"}
+return {'HiPhish/rainbow-delimiters.nvim', version = "0.7.0"}
