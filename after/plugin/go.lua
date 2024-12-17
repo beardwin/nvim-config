6045050419f491f99bdb976c5b37bb9318c53d17
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-12-16 16:33:03 -0800

    remove unused local

--- after/plugin/go.lua
+++ after/plugin/go.lua
@@ -1,7 +1,5 @@
 require('go').setup()
 
-local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-
 vim.keymap.set({ 'n' }, '<C-g>b', ':GoBuild ', { noremap = true, desc = "Run go build" })
 vim.keymap.set({ 'n', 'i' }, '<C-g>f', ':GoFmt<cr>', { noremap = true, desc = "Go format" })
 vim.keymap.set({ 'n', 'i' }, '<C-g>d', ':GoDoc<cr>', { noremap = true, desc = "Open the Go docs for this" })
