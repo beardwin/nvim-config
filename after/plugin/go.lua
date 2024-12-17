61d344a9a14056ee24f2c54d043686130cfc279e
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2024-12-16 16:31:15 -0800

    add conform.nvim

--- after/plugin/go.lua
+++ after/plugin/go.lua
@@ -1,20 +1,9 @@
 require('go').setup()
 
--- Run gofmt + goimports on save
-
 local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
-
-vim.keymap.set({'n'}, '<C-g>b', ':GoBuild ', {noremap = true, desc = "Run go build"})
-vim.keymap.set({'n', 'i'}, '<C-g>f', ':GoFmt<cr>', {noremap = true, desc = "Go format"})
-vim.keymap.set({'n', 'i'}, '<C-g>d', ':GoDoc<cr>', {noremap = true, desc = "Open the Go docs for this"})
-vim.keymap.set({'n', 'i'}, '<C-g>o', ':GoPkgOutline<cr>', {noremap = true, desc = "Toggle the Go package outline"})
-vim.keymap.set({'n', 'i'}, '<C-g>i', ':GoImpl', {noremap = true, desc = "Run go impl"})
-
+vim.keymap.set({ 'n' }, '<C-g>b', ':GoBuild ', { noremap = true, desc = "Run go build" })
+vim.keymap.set({ 'n', 'i' }, '<C-g>f', ':GoFmt<cr>', { noremap = true, desc = "Go format" })
+vim.keymap.set({ 'n', 'i' }, '<C-g>d', ':GoDoc<cr>', { noremap = true, desc = "Open the Go docs for this" })
+vim.keymap.set({ 'n', 'i' }, '<C-g>o', ':GoPkgOutline<cr>', { noremap = true, desc = "Toggle the Go package outline" })
+vim.keymap.set({ 'n', 'i' }, '<C-g>i', ':GoImpl', { noremap = true, desc = "Run go impl" })
